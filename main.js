70936f5257abd96a94fac0febc047342f7009864
M1chol <106252516+M1chol@users.noreply.github.com>
2022-05-26 01:05:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 var history = document.getElementById("history");
 var input = document.getElementById("input");
+var path = document.getElementById("path");
+var user = "m1-all\\guest> ";
+var used = [];
+title1 = [
+    "&nbsp;&nbsp;&nbsp;__  ______    ___   __   __                  __",
+    "&nbsp;&nbsp;/  |/  <  /___/ _ | / /  / /    ______ _  ___/ /",
+    "&nbsp;/ /|_/ // /___/ __ |/ /__/ /__  / __/  ' \\/ _  /",
+    "/_/  /_//_/   /_/ |_/____/____/  \\__/_/_/_/\\_,_/",
+    'Welcome to m1-all terminal start by checking "help" ',
+    '<br>'
+    ]
+
+path.innerHTML = user;
+loopLines(title1,"orange")
 
 input.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
+    if (event.key == "Enter") {
         console.log("Executing", input.value);
+        addLine(user + input.value, "no-anim");
         command(input.value);
         input.value = "";
     }
@@ -12,33 +27,52 @@ input.addEventListener("keypress", function (event) {
 
 function command(cmd) {
     switch (cmd.toLowerCase()) {
+        case '':
+            break;
         case 'help':
-            addLine('Full list of commands: [help, clear, about, projects, contact]');
+            loopLines(help_prt)
             break;
         case 'clear':
             document.getElementById('history').innerHTML = '';
             break;
         case 'about':
-            addLine('Website made by Michal Kozlowski as portfolio, check contact for more');
+            addLine('Website made by Michał Kozłowski as portfolio, check contact for more', "txtcmd");
             break;
         case 'projects':
-            addLine('Full list of projects: [RC autonomus car, Spudgun, and more]');
+            addLine('Full list of projects: [RC autonomus car, Spudgun, and more]', "txtcmd");
             break;
         case 'contact':
-            addLine('GitHub: https://github.com/M1chol')
+            addLine('Insta: www.instagram.com/kolega__michal/, Git: github.com/M1chol')
+            break;
+        case 'title':
+            loopLines(title1, "orange")
             break;
 
         default:
-            addLine('Command not found type "help" for list of commands');
+            addLine('<span class=\"red">Command not found, type "help" for list of commands</span>', "txtcmd");
 
     }
     console.log(input.value, "executed")
 }
 
-function addLine(text) {
-    txt = document.createElement("p");
-    textNode = document.createTextNode(text);
-    txt.appendChild(textNode);
+function addLine(text, style) {
+    var txt = document.createElement("p");
+    txt.innerHTML = text;
+    txt.className = style;
     document.getElementById('history').appendChild(txt);
     
+}
+function loopLines(text, style) {
+    text.forEach(function (item) {
+        var t = "";
+        for (let i = 0; i < item.length; i++) {
+            if (item.charAt(i) == " " && item.charAt(i + 1) == " ") {
+                t += "&nbsp;&nbsp;";
+                i++;
+            } else {
+                t += item.charAt(i);
+            }
+        }
+        addLine(t, style);
+    });
 }
