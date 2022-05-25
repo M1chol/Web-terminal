dda133ac367752a6bff00f771d753528e3723614
M1chol <106252516+M1chol@users.noreply.github.com>
2022-05-26 01:07:13 +0200

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,10 @@
+help_prt = [
+    '<br>',
+    'help               list of commands',
+    'clear              clears terminal',
+    'about              what is this?',
+    'projects           my all projects',
+    'contact            pretty obvious',
+    'title              fancy title',
+    '<br>'
+]
