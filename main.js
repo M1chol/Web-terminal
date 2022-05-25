5e9b3a3d43ebcfd3f225850db1efa54872156fab
M1chol <106252516+M1chol@users.noreply.github.com>
2022-05-25 15:32:53 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var history = document.getElementById("history");
+var input = document.getElementById("input");
+
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        console.log("Executing", input.value);
+        command(input.value);
+        input.value = "";
+    }
+});
+
+
+function command(cmd) {
+    switch (cmd.toLowerCase()) {
+        case 'help':
+            addLine('Full list of commands: [help, clear, about, projects, contact]');
+            break;
+        case 'clear':
+            document.getElementById('history').innerHTML = '';
+            break;
+        case 'about':
+            addLine('Website made by Micha³ Koz³owski as portfolio, check contact for more');
+            break;
+        case 'projects':
+            addLine('Full list of projects: [RC autonomus car, Spudgun, and more]');
+            break;
+        case 'contact':
+            addLine('Insta: https//www.instagram.com/kolega__michal/')
+            break;
+
+        default:
+            addLine('Command not found type "help" for list of commands');
+
+    }
+    console.log(input.value, "executed")
+}
+
+function addLine(text) {
+    txt = document.createElement("p");
+    textNode = document.createTextNode(text);
+    txt.appendChild(textNode);
+    document.getElementById('history').appendChild(txt);
+    
+}
